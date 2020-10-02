ae93b179533b20b312cba1778a80aaca92b8ac8e
kate roy <kroy15chs@gmail.com>
2020-10-02 13:17:34 -0400

    MVP

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,39 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<nav></nav>)
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm/>)
+
+    const inputFirstName = screen.getByLabelText(/first name/i)
+    const inputLastName = screen.getByLabelText(/last name/i)
+    const inputAddress = screen.getByLabelText(/address/i)
+    const inputCity = screen.getByLabelText(/city/i)
+    const inputState = screen.getByLabelText(/state/i)
+    const inputZip = screen.getByLabelText(/zip/i)
+
+    fireEvent.change(inputFirstName, { target: { value:'Kate' } })
+    fireEvent.change(inputLastName, { target: { value:'Roy' } })
+    fireEvent.change(inputAddress, { target: { value:'Jasmine Dragon' } })
+    fireEvent.change(inputCity, { target: { value:'Upper Ring' } })
+    fireEvent.change(inputState,{ target: { value:'Ba Sing Se'}})
+    fireEvent.change(inputZip, { target: { value:'09856' } })
+
+    const checkoutButton = screen.getByRole('button',/checkout/i)
+
+    fireEvent.click(checkoutButton)
+
+    expect(screen.getByTestId(/successMessage/i)).toBeInTheDocument()
+    expect(screen.getByText(/Kate/i)).toBeInTheDocument()
+    expect(screen.getByText(/Roy/i)).toBeInTheDocument()
+    expect(screen.getByText(/Jasmine Dragon/i)).toBeInTheDocument()
+    expect(screen.getByText(/Upper Ring/i)).toBeInTheDocument()
+    expect(screen.getByText(/Ba Sing Se/i)).toBeInTheDocument()
+    expect(screen.getByText(/09856/i)).toBeInTheDocument()
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,16 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react'
+
+const useForm = (initialValue) => {
+    const [values, setValues] = useState(initialValue)
+
+    const handleChanges = evt => {
+        setValues({
+            ...values,
+            [evt.target.name]: evt.target.value
+        })
+    }
+    return [values, handleChanges]
+}
+
+export default useForm
